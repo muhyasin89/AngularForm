3a4c3cab87c98736607ad33112b10b6d57978c7d
muhyasin89 <muhyasin89@gmail.com>
2022-11-27 11:15:33 +0700

    [master]add submit data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
 import { User } from './user';
 
 @Component({
@@ -10,4 +11,26 @@ export class AppComponent {
   title = 'tdf';
   topics =["Angular", "React", "Vue"];
   userModel = new User('Rob', 'rob@test.com', 3332224455, '', 'morning', true);
+
+  topicHasError = true;
+
+  constructor(private _enrollmentService:EnrollmentService);
+
+  validateTopic(value: string){
+    if(value == 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+    }
+
+  }
+  
+
+  onSubmit(){
+    this._enrollmentService.enroll(this.userModel).subscribe(
+      data => console.log('Success!'),
+      error => console.error('Error!', error)
+    )
+    // console.log(this.userModel);
+  }
 }
